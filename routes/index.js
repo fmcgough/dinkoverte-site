517449efd939ca8f5be7209e54b0f49a87306bd0
Frankie McGough <fmcgough@live.co.uk>
2014-02-03 23:25:14 +0000

    Added 'about' and 'contact' pages (no content... yet) Changed the navbar colour to match the dinkoverte brand Made the navbar highlight the active page

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,20 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Dinkoverte' });
+  res.render('index', 
+    { title: 'Dinkoverte', 
+    navbarActive : 'Home' });
 };
+
+// GET 'About' page.
+exports.about = function(req, res) {
+    res.render('about', 
+        {title :'Dinkoverte - About', 
+        navbarActive: 'About'});
+}
+
+exports.contact = function (req, res) {
+    res.render('contact', 
+        {title : 'Dinkoverte - Contact', 
+        navbarActive: 'Contact'});
+}
